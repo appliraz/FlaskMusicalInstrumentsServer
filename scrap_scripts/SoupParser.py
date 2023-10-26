51119f59554440c059dbe900bc077887a0c4f378
appliraz <114522959+appliraz@users.noreply.github.com>
2023-10-26 23:07:09 +0300

    Update SoupParser.py

--- scrap_scripts/SoupParser.py
+++ scrap_scripts/SoupParser.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import configurations.variablesService as vs
-import ProductPageScrap
+import scrap_scripts.ProductPageScrap
 
 def getRelevantSoup(soup: BeautifulSoup, website_configs: dict):
     main_website_tag = website_configs[vs.main_html_element]['tag']
