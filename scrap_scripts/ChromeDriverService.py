3d2ea8f3de6b213c4151b2b9fdbf07c9d272a4a5
appliraz <114522959+appliraz@users.noreply.github.com>
2023-10-27 11:00:09 +0300

    Update ChromeDriverService.py

--- scrap_scripts/ChromeDriverService.py
+++ scrap_scripts/ChromeDriverService.py
@@ -6,10 +6,28 @@ from selenium import webdriver
 
 
 
+"""
 def getChromeOptions():
     chrome_options = Options()
     chrome_options.add_argument('--headless')  # Enable headless mode
     chrome_options.add_argument('--disable-gpu')  # Disable GPU acceleration (may be required in some cases)
+    return chrome_options
+
+"""
+def getChromeOptions():
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-crash-reporter")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-in-process-stack-traces")
+    chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--output=/dev/null")
+
     return chrome_options
 
 #chrome_driver = ChromeDriverManager().install()
