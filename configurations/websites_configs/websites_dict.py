e74fdcbec733c0de6dd791ca67ee6f83f026dc31
appliraz <114522959+appliraz@users.noreply.github.com>
2023-10-28 15:03:13 +0300

    Add files via upload

--- configurations/websites_configs/websites_dict.py
+++ configurations/websites_configs/websites_dict.py
@@ -1,4 +1,4 @@
-from . import halilit_configs, avigil_configs, diez_configs, kleyzemer_configs, musiccenter_configs, wildguitars_configs, kzpro_configs
+from . import halilit_configs, avigil_configs, diez_configs, kleyzemer_configs, musiccenter_configs, wildguitars_configs, kzpro_configs, avi_gil_configs
 
 websites_dict = {
     halilit_configs.website_key: halilit_configs.website_configs,
@@ -7,5 +7,6 @@ websites_dict = {
     kleyzemer_configs.website_key: kleyzemer_configs.website_configs,
     musiccenter_configs.website_key: musiccenter_configs.website_configs,
     wildguitars_configs.website_key: wildguitars_configs.website_configs,
-    kzpro_configs.website_key: kzpro_configs.website_configs
+    kzpro_configs.website_key: kzpro_configs.website_configs,
+    avi_gil_configs.website_key: avi_gil_configs.website_configs
 }
